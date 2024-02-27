49652f87fd3c31a569cfc8dfe39b140dd5043508
Sandro Schmid <saseb.schmid@gmail.com>
2024-02-27 13:22:41 +0100

    Remove unused analytics dependency, use pnpm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/alpine
   extends: '@nuxt-themes/alpine',
-
   modules: [
-    // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible',
     // https://github.com/nuxt/devtools
     '@nuxt/devtools'
   ]
